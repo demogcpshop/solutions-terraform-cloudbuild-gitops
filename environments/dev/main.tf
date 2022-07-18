484822d7e72159f18537245482076e57d42f085c
demogcpshop <demogcpshop@gmail.com>
2022-07-18 16:03:29 +0000

    Adding new compute resource

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -38,3 +38,9 @@ module "firewall" {
   project = "${var.project}"
   subnet  = "${module.vpc.subnet}"
 }
+
+module "compute" {
+  source  = "../../modules/compute"
+  project = "${var.project}"
+  subnet  = "${module.vpc.subnet}"
+}
