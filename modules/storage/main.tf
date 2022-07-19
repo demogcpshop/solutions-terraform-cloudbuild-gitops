59922a3e591d40a3c8294ea52ab80943f2d6797d
demogcpshop <demogcpshop@gmail.com>
2022-07-19 09:47:01 +0000

    Add storage bucket

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,8 +1,8 @@
 resource "google_storage_bucket" "test-bucket3245" {
   force_destroy               = false
-  location                    = "#${var.region}"
-  name                        = "#${var.env}-test-bucket3245"
-  project                     = "#${var.project}"
+  location                    = "${var.region}"
+  name                        = "${var.env}-test-bucket3245"
+  project                     = "${var.project}"
   #public_access_prevention    = "enforced"
   storage_class               = "STANDARD"
   uniform_bucket_level_access = true
