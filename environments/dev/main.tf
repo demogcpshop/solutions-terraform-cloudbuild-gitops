b622e5e2d73857ed3cff56dfd89e912d956e6ceb
demogcpshop <demogcpshop@gmail.com>
2022-07-28 14:23:51 +0000

    Adding test compute instance

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -38,10 +38,6 @@ module "firewall" {
   subnet  = "${module.vpc.subnet}"
 }
 
-module "testCompute" {
-  source  = "../../modules/testCompute"
-}
-
 #module "notebook" {
 #  source  = "../../modules/vertex_ai"
 #  project = "${var.project}"
