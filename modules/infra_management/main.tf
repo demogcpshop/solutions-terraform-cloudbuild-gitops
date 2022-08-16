86a462ed5b91d767099c5970d748812635f97b55
demogcpshop <demogcpshop@gmail.com>
2022-08-16 02:19:10 +0000

    adding infra resources

--- modules/infra_management/main.tf
+++ modules/infra_management/main.tf
@@ -1,24 +1,24 @@
-resource "google_service_account" "sa-name" {
-  #account_id = "sa-name"
+resource "google_service_account" "sa-infra" {
+  account_id = "sa-infra"
   display_name = "mtn-adam-${var.OPCO}-${var.USE_CASE}"
 }
 
 resource "google_project_iam_member" "infra_role_clouddeploy" {
   #project = <your_gcp_project_id_here>
   role    = "roles/clouddeploy.admin"
-  member  = "serviceAccount:${google_service_account.sa-name.email}"
+  member  = "serviceAccount:${google_service_account.sa-infra.email}"
 }
 
 resource "google_project_iam_member" "infra_role_project" {
   #project = <your_gcp_project_id_here>
   role    = "roles/projectbilling.manager"
-  member  = "serviceAccount:${google_service_account.sa-name.email}"
+  member  = "serviceAccount:${google_service_account.sa-infra.email}"
 }
 
 resource "google_project_iam_member" "infra_role_editor" {
   #project = <your_gcp_project_id_here>
   role    = "roles/editor"
-  member  = "serviceAccount:${google_service_account.sa-name.email}"
+  member  = "serviceAccount:${google_service_account.sa-infra.email}"
 }
 
 resource "google_storage_bucket" "eva" {
@@ -44,5 +44,5 @@ resource "google_storage_bucket" "tf-state" {
 resource "google_storage_bucket_iam_member" "member" {
   bucket = google_storage_bucket.default.name
   role = "roles/storage.admin"
-  member = "serviceAccount:${google_service_account.sa-name.email}"
+  member = "serviceAccount:${google_service_account.sa-infra.email}"
 }
