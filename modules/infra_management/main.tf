4430492d543223f7f0a620d56a0610e16ea356f7
demogcpshop <demogcpshop@gmail.com>
2022-08-16 02:12:00 +0000

    adding infra resources

--- modules/infra_management/main.tf
+++ modules/infra_management/main.tf
@@ -3,30 +3,30 @@ resource "google_service_account" "sa-name" {
   display_name = "mtn-adam-${var.opco}-${var.USE_CASE}"
 }
 
-resource "google_project_iam_member" "firestore_owner_binding" {
+resource "google_project_iam_member" "infra_role_clouddeploy" {
   #project = <your_gcp_project_id_here>
   role    = "roles/clouddeploy.admin"
   member  = "serviceAccount:${google_service_account.sa-name.email}"
 }
 
-resource "google_project_iam_member" "firestore_owner_binding" {
+resource "google_project_iam_member" "infra_role_project" {
   #project = <your_gcp_project_id_here>
   role    = "roles/projectbilling.manager"
   member  = "serviceAccount:${google_service_account.sa-name.email}"
 }
 
-resource "google_project_iam_member" "firestore_owner_binding" {
+resource "google_project_iam_member" "infra_role_editor" {
   #project = <your_gcp_project_id_here>
   role    = "roles/editor"
   member  = "serviceAccount:${google_service_account.sa-name.email}"
 }
 
-resource "google_storage_bucket" "auto-expire" {
+resource "google_storage_bucket" "eva" {
   name          = "mtn-adam-${var.opco}-${var.USE_CASE}-bucket"
   location      = "US"
 }
 
-resource "google_storage_bucket" "auto-expire" {
+resource "google_storage_bucket" "tf-state" {
   name          = "mtn-adam-infra-state-bucket"
   location      = "US"
 
