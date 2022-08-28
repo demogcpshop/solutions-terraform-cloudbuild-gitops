db323198e35761311c91e81e4290a2701a50ac9f
demogcpshop <demogcpshop@gmail.com>
2022-08-28 22:17:02 +0000

    Add infra

--- modules/infra_management/main.tf
+++ modules/infra_management/main.tf
@@ -3,11 +3,11 @@ resource "google_service_account" "sa-infra" {
   display_name = "mtn-adam-${var.OPCO}-${var.USE_CASE}"
 }
 
-resource "google_project_iam_member" "infra_role_clouddeploy" {
-  #project = <your_gcp_project_id_here>
-  role    = "roles/appengine.deployer"
-  member  = "serviceAccount:${google_service_account.sa-infra.email}"
-}
+# resource "google_project_iam_member" "infra_role_clouddeploy" {
+#   #project = <your_gcp_project_id_here>
+#   role    = "roles/appengine.deployer"
+#   member  = "serviceAccount:${google_service_account.sa-infra.email}"
+# }
 
 resource "google_storage_bucket" "eva" {
   name          = "mtn-adam-${var.OPCO}-${var.USE_CASE}-bucket"
