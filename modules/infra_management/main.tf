2573e1bc0d56e0a596d91552610399cacbf7aa55
demogcpshop <demogcpshop@gmail.com>
2022-08-29 12:04:20 +0000

    Add infra

--- modules/infra_management/main.tf
+++ modules/infra_management/main.tf
@@ -5,7 +5,7 @@ resource "google_service_account" "sa" {
 
 resource "google_service_account_iam_binding" "admin-account-iam" {
   service_account_id = google_service_account.sa.name
-  role               = "roles/iam.serviceAccounts.setIamPolicy"
+  role               = "roles/iam.serviceAccount.admin"
 
   members = [
     "user:demogcpshop@gmail.com",
