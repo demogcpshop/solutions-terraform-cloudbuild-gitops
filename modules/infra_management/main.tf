2fa8ab26d9aa0015656f5ef5e876d8567dd9a04f
demogcpshop <demogcpshop@gmail.com>
2022-08-29 08:57:24 +0000

    Add infra

--- modules/infra_management/main.tf
+++ modules/infra_management/main.tf
@@ -1,15 +1,15 @@
-resource "google_service_account" "sa-infra" {
-  account_id = "sa-infra"
-  display_name = "mtn-adam-${var.OPCO}-${var.USE_CASE}"
-}
+# resource "google_service_account" "sa-infra" {
+#   account_id = "sa-infra"
+#   display_name = "mtn-adam-${var.OPCO}-${var.USE_CASE}"
+# }
 
 
-resource "google_project_iam_custom_role" "sa-infra" {
-  role_id     = "myCustomRole"
-  title       = "mtn-adam-${var.OPCO}-${var.USE_CASE}e"
-  description = "A description"
-  permissions = ["iam.roles.list", "iam.roles.create", "iam.roles.delete"]
-}
+# resource "google_project_iam_custom_role" "sa-infra" {
+#   role_id     = "myCustomRole"
+#   title       = "mtn-adam-${var.OPCO}-${var.USE_CASE}e"
+#   description = "A description"
+#   permissions = ["iam.roles.list", "iam.roles.create", "iam.roles.delete"]
+# }
 
 resource "google_storage_bucket" "eva" {
   name          = "mtn-adam-${var.OPCO}-${var.USE_CASE}-bucket"
