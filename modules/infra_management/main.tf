dde1e66a1550b7d90067068e8f33dce6b8ed9df0
demogcpshop <demogcpshop@gmail.com>
2022-08-29 09:36:36 +0000

    Add infra

--- modules/infra_management/main.tf
+++ modules/infra_management/main.tf
@@ -4,11 +4,24 @@
 # }
 
 
-resource "google_project_iam_custom_role" "sa-infra" {
-  role_id     = "myCustomRole"
-  title       = "mtn-adam-${var.OPCO}-${var.USE_CASE}e"
-  description = "A description"
-  permissions = ["appengine.applications.update", "appengine.instances.delete", "appengine.instances.get"]
+# resource "google_project_iam_custom_role" "sa-infra" {
+#   role_id     = "myCustomRole"
+#   title       = "mtn-adam-${var.OPCO}-${var.USE_CASE}e"
+#   description = "A description"
+#   permissions = ["appengine.applications.update", "appengine.instances.delete", "appengine.instances.get"]
+# }
+
+resource "google_service_account" "log_user" {
+  account_id   = "sa-infra"
+  display_name = "mtn-adam-${var.OPCO}-${var.USE_CASE}"
+}
+
+resource "google_project_iam_binding" "sa-infra" {
+  project = "norse-ward-356309"
+  role    = "roles/logging.logWriter"
+  members = [
+    "serviceAccount:${google_service_account.sa-infra.email}"
+  ]
 }
 
 resource "google_storage_bucket" "eva" {
