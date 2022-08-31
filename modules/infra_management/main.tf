f63085a786dd47dfa25a60600366e86d339d2d71
demogcpshop <demogcpshop@gmail.com>
2022-08-31 21:23:14 +0000

    Adding infa

--- modules/infra_management/main.tf
+++ modules/infra_management/main.tf
@@ -1,14 +1,13 @@
 resource "google_service_account" "sa-name" {
   account_id = "sa-name"
   display_name = "SA"
+}
 
   resource "google_project_iam_member" "firestore_owner_binding2" {
   project = "norse-ward-356309"
   role    = "roles/owner"
   member  = "serviceAccount:${google_service_account.sa-name.email}"
 }
-}
-
 
 resource "google_project_iam_member" "firestore_owner_binding" {
   project = "norse-ward-356309"
