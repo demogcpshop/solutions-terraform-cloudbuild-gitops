d5273a3e1e08bda43539334724b88aa98272f1c2
demogcpshop <demogcpshop@gmail.com>
2022-08-31 20:48:52 +0000

    Adding infa

--- modules/infra_management/main.tf
+++ modules/infra_management/main.tf
@@ -1,27 +1,36 @@
-resource "google_service_account" "sa" {
-  account_id   = "my-service-account"
-  display_name = "A service account that only Jane can use"
+# resource "google_service_account" "sa" {
+#   account_id   = "my-service-account"
+#   display_name = "A service account that only Jane can use"
+# }
+
+# output "serviceaccountid" {
+#   value = google_service_account.sa.id
+# }
+
+# resource "google_service_account_iam_binding" "admin-account-iam" {
+#   #service_account_id = google_service_account.sa.name
+#   service_account_id = google_service_account.sa.id
+#   role               = "roles/aiplatform.serviceAgent"
+
+#   members = [
+#     #"user:demogcpshop@gmail.com",
+#      "serviceAccount:${google_service_account.sa.email}" 
+#   ]
+
+#   depends_on = [
+#     google_service_account.sa
+#   ]
+# }
+resource "google_service_account" "sa-name" {
+  account_id = "sa-name"
+  display_name = "SA"
 }
 
-output "serviceaccountid" {
-  value = google_service_account.sa.id
+resource "google_project_iam_member" "firestore_owner_binding" {
+  project = "norse-ward-356309"
+  role    = "roles/datastore.owner"
+  member  = "serviceAccount:${google_service_account.sa-name.email}"
 }
-
-resource "google_service_account_iam_binding" "admin-account-iam" {
-  #service_account_id = google_service_account.sa.name
-  service_account_id = google_service_account.sa.id
-  role               = "roles/aiplatform.serviceAgent"
-
-  members = [
-    #"user:demogcpshop@gmail.com",
-     "serviceAccount:${google_service_account.sa.email}" 
-  ]
-
-  depends_on = [
-    google_service_account.sa
-  ]
-}
-
 
 resource "google_storage_bucket" "eva" {
   name          = "mtn-adam-${var.OPCO}-${var.USE_CASE}-bucket"
