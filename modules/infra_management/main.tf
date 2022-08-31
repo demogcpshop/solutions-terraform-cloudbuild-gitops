166ce15331a213d6e902edfe43ad7ca3336824bc
demogcpshop <demogcpshop@gmail.com>
2022-08-31 21:17:19 +0000

    Adding infa

--- modules/infra_management/main.tf
+++ modules/infra_management/main.tf
@@ -1,42 +1,32 @@
-# resource "google_service_account" "sa" {
-#   account_id   = "my-service-account"
-#   display_name = "A service account that only Jane can use"
-# }
-
-# output "serviceaccountid" {
-#   value = google_service_account.sa.id
-# }
-
-# resource "google_service_account_iam_binding" "admin-account-iam" {
-#   #service_account_id = google_service_account.sa.name
-#   service_account_id = google_service_account.sa.id
-#   role               = "roles/aiplatform.serviceAgent"
-
-#   members = [
-#     #"user:demogcpshop@gmail.com",
-#      "serviceAccount:${google_service_account.sa.email}" 
-#   ]
-
-#   depends_on = [
-#     google_service_account.sa
-#   ]
-# }
-resource "google_service_account" "sa-name" {
-  account_id = "sa-name"
-  display_name = "SA"
+resource "google_service_account" "log_user" {
+  account_id   = "log-user"
+  display_name = "Logging User"
 }
 
-resource "google_project_iam_member" "firestore_owner_binding2" {
-  project = "norse-ward-356309"
-  role    = "roles/owner"
-  member  = "serviceAccount:${google_service_account.sa-name.email}"
+resource "google_project_iam_binding" "log_user" {
+  project = "arcadia-apps-237918"
+  role    = "roles/logging.logWriter"
+  members = [
+    "serviceAccount:${google_service_account.log_user.email}"
+  ]
 }
 
-resource "google_project_iam_member" "firestore_owner_binding" {
-  project = "norse-ward-356309"
-  role    = "roles/datastore.owner"
-  member  = "serviceAccount:${google_service_account.sa-name.email}"
-}
+# resource "google_service_account" "sa-name" {
+#   account_id = "sa-name"
+#   display_name = "SA"
+# }
+
+# resource "google_project_iam_member" "firestore_owner_binding2" {
+#   project = "norse-ward-356309"
+#   role    = "roles/owner"
+#   member  = "serviceAccount:${google_service_account.sa-name.email}"
+# }
+
+# resource "google_project_iam_member" "firestore_owner_binding" {
+#   project = "norse-ward-356309"
+#   role    = "roles/datastore.owner"
+#   member  = "serviceAccount:${google_service_account.sa-name.email}"
+# }
 
 resource "google_storage_bucket" "eva" {
   name          = "mtn-adam-${var.OPCO}-${var.USE_CASE}-bucket"
